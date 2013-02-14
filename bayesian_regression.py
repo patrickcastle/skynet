c2d2df35309721638e5d98f9e322533976be92b3
Patrick McGuire <patrick@castleridgemedia.com>
2013-02-14 18:25:31 -0500

    not sure if this is working yet

--- bayesian_regression.py
+++ bayesian_regression.py
@@ -3,14 +3,22 @@ import numpy.linalg
 import numpy.random
 
 
-def fit(target, training, mean=None, covariance=None):
+def fit(target, training, mean=None, covariance=None, noise=20):
     if None == mean:
         mean = numpy.zeros(training.shape[1])
     if None == covariance:
-        covariance = numpy.ident(traiing.shape[1])
+        covariance = numpy.identity(training.shape[1])
 
-    covariance_next = covariance + numpy.dot(training.T, training)
-    mean_next = numpy.dot(numpy.linalg.convariance_next, (numpy.dot(covariance, mean) + numpy.dot(example.T, target)))
+    print "target " + str(target)
+
+    covariance_next = numpy.linalg.inv(numpy.linalg.inv(covariance) + noise * numpy.dot(training.T, training))
+    print "training shape " + str(training.T.shape)
+    print "target shape " + str(target.shape)
+    print "covariance shape " + str(covariance.shape)
+    print "covariance next " + str(covariance_next.shape)
+    mean_next = numpy.dot(covariance_next, (numpy.dot(numpy.linalg.inv(covariance), mean) + noise * numpy.dot(training.T, target)))
+    
+    numpy.dot(covariance_next, numpy.dot(covariance, mean) + numpy.dot(target.T, target))
     
     def fit_apply(test):
         numpy.dot(mean_next, test.T)
--- time_series.py
+++ time_series.py
@@ -1,6 +1,11 @@
 import time
 import numpy
 import csv
+import scipy.optimize
+import matplotlib
+import matplotlib.pyplot
+import bayesian_regression
+
 date_format = '%Y-%m-%d %H:%M:%S'
 default_keys = ['start_time','hour','impressions','campaign_id','campaign']
 
@@ -15,23 +20,23 @@ def parse(iterable, keys=default_keys):
     lists = {}
 
     try:
-        start_time_index = default_keys.index('start_time')
+        start_time_index = keys.index('start_time')
     except:
         start_time_index = None
     try:
-        hour_index = default_keys.index('hour')
+        hour_index = keys.index('hour')
     except:
         hour_index = None
     try:
-        impressions_index = default_keys.index('impressions')
+        impressions_index = keys.index('impressions')
     except:
         impressions_index = None
     try:
-        campaign_id_index = default_keys.index('campaign_id')
+        campaign_id_index = keys.index('campaign_id')
     except:
         campaign_id_index = None
     try:
-        campaign_index = default_keys.index('campaign')
+        campaign_index = keys.index('campaign')
     except:
         campaign_index = None
 
@@ -42,22 +47,22 @@ def parse(iterable, keys=default_keys):
         try:
             line_list = []
             campaign = None
-            if start_time_index:
+            if None != start_time_index:
                 start_time = time.strptime(line[start_time_index], date_format)
                 line_list.append(start_time)
-            if hour_index:
+            if None != hour_index:
                 hour = time.strptime(line[hour_index], date_format)
                 line_list.append(hour)
-            if impressions_index:
+            if None != impressions_index:
                 impressions = line[impressions_index]
                 line_list.append(impressions)
-            if campaign_id_index:
+            if None != campaign_id_index:
                 campaign_id = line[campaign_id_index]
                 line_list.append(campaign_id)
-            if campaign_index:
+            if None != campaign_index:
                 campaign = line[campaign_index]
                 line_list.append(campaign)
-            if campaign:
+            if None != campaign:
                 if not campaign in lists:
                     lists[campaign] = []
                 lists[campaign].append(line_list)
@@ -164,3 +169,75 @@ def offset_factors(filename, keys=default_keys):
 
 def make_lag_vector(filename):
     res = load_it_all(filename)
+
+
+def find_and_graph_decay(observations):
+    decay = find_decay(observations)
+    graph_decay(decay, observations)
+
+
+def find_decay(observations):
+    nonzero_indices = numpy.where(observations!=0)[0]
+    nonzero = observations[nonzero_indices]
+
+    num_observations = nonzero.shape[0]
+    design = numpy.ones((num_observations,2)) # intercept and decay
+    for i in range(num_observations):
+        design[i,1] = nonzero_indices[i]
+    logged = numpy.log(nonzero) # to prevent NaN all over the goddamn place
+    return numpy.linalg.lstsq(design, logged)[0]
+
+
+def graph_decay(decay, observations):
+    line = numpy.zeros(observations.shape[0])
+    for i, o in enumerate(observations):
+        line[i] = numpy.exp(decay[0] + i * decay[1])
+    matplotlib.pyplot.plot(line)
+    matplotlib.pyplot.plot(observations)
+    matplotlib.pyplot.show()
+
+
+def find_decay_function(impression_matrix, kernel=(0,0,0)):
+    def kernel_error(kernel):
+        errors = []
+        for i, row in enumerate(impression_matrix):
+            for column_index, column in enumerate(row):
+                expected = kernel[0]
+                for j in range(1, len(kernel)):
+                    expected += kernel[j] * column_index ** j
+                errors.append((expected - column) ** 2)
+
+        print (kernel, sum(errors))
+        return sum(errors)
+
+    return scipy.optimize.minimize(kernel_error, kernel)
+
+
+def find_reasonable_decay_length(observations):
+    nonzero_indices = numpy.where(observations!=0)[0]
+    nonzero = observations[nonzero_indices]
+    logged = numpy.log(nonzero)
+
+    design = numpy.ones((nonzero_indices.shape[0],2))
+    for i in range(nonzero_indices.shape[0]):
+        design[i,1] = nonzero_indices[i]
+
+    mean = numpy.array((1,1))
+    covariance = numpy.identity(2)
+    fit_apply = None
+    for i, observation in enumerate(logged):
+        print (mean, covariance)
+        print "should be " + str(numpy.array([logged[i]]).shape)
+        fit_apply, mean, covariance = bayesian_regression.fit(numpy.array([logged[i]]), design[i].reshape(1,2), mean, covariance)
+    return fit_apply, mean, covariance
+
+def find_reasonable_and_graph(observations):
+    decay = find_reasonable_decay_length(observations)[1]
+    nonzero = observations[numpy.where(observations!=0)]
+    graph_decay(decay, nonzero)
+
+def test_parse():
+    parsed = fileparse('series.csv')
+    offsetted_vals = offsetted(parsed, relevant_columns=[1])
+    return offsetted_vals
+    
